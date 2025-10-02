3ba22ea41624b5f20011ab284444115deb89808a
scyaena <smpurisima@gbox.adnu.edu.ph>
2025-10-01 20:25:22 -0700

    PROBLEM2.cpp

---
+++ ACTIVITY4/PROBLEM2.cpp
@@ -0,0 +1,20 @@
+Problem 2: Weather-Based Discounts
+
+Problem:
+You want to attract more customers. If it’s hot (temperature is 30°C or more), offer a 20% discount.
+
+Task:
+Write a program that:
+Ask for the temperature.
+
+Sets the price to:
+$0.80 if temperature >= 30
+$1.00 otherwise
+Outputs the final price.
+
+Sample Input:
+Enter today's temperature in Celsius: 32
+
+Sample Output:
+
+It's hot! Lemonade costs $0.80 today.
