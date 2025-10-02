d1371a98b5a24116567dfe9b27b2c8df130df89c
scyaena <smpurisima@gbox.adnu.edu.ph>
2025-10-01 20:24:41 -0700

    PROBLEM1.cpp

--- ACTIVITY4/PROBLEM1.cpp
+++ ACTIVITY4/PROBLEM1.cpp
@@ -1,1 +1,19 @@
-act4
+Problem 1: Basic Pricing Decision
+
+Problem:
+You sell one cup of lemonade for $1. If a customer has $1 or more, they can buy a cup.
+
+Task:
+Write a program that:
+Ask the user how much money they have.
+Uses an if statement to determine if they can buy lemonade.
+
+Outputs:
+"Enjoy your lemonade!" if they have $1 or more.
+"Sorry, you need more money." otherwise.
+
+Sample Input:
+Enter your money: 0.75
+
+Sample Output:
+Sorry, you need more money.
