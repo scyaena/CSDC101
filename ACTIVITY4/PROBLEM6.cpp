85b270b73a9d2579fc62f42b4edbdca3fe686b6d
scyaena <smpurisima@gbox.adnu.edu.ph>
2025-10-05 21:13:05 +0800

    PROBLEM6.cpp

--- ACTIVITY4/PROBLEM6.cpp
+++ ACTIVITY4/PROBLEM6.cpp
@@ -2,28 +2,28 @@
 using namespace std;
 
 int main() {
-    int problem;
+    int problem;  // variable to store the user's selected problem number
     cout << "What problem do you want to see? ";
-    cin >> problem;
+    cin >> problem; // get input from user
 
-    switch (problem) {
-        case 1:
+    switch (problem) { // to determine which problem to show
+        case 1: // when user inputs 1
             cout << "Basic Pricing Decision Runs" << endl;
             break;
-        case 2:
+        case 2: // when user inputs 2
             cout << "Discount Calculator" << endl;
             break;
-        case 3:
+        case 3: // when user inputs 3
             cout << "Inventory Check" << endl;
             break;
-        case 4:
+        case 4: // when user inputs 4
             cout << "Temperature Converter" << endl;
             break;
-        case 5:
+        case 5: // when user inputs 5
             cout << "Player Movement" << endl;
             break;
-        default:
-            cout << "Invalid problem number!" << endl;
+        default: // runs when the user enters a number not between 1–5
+            cout << "Invalid problem number" << endl;
     }
 
     return 0;
