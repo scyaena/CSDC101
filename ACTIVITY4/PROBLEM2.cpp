2c5b1994a29084713807e60e6d5de2cdd370db46
scyaena <smpurisima@gbox.adnu.edu.ph>
2025-10-05 22:19:25 +0800

    PROBLEM2.cpp

--- ACTIVITY4/PROBLEM2.cpp
+++ ACTIVITY4/PROBLEM2.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
     int temperature;
 
-    cout << "Enter today's temperature in Celsius: ";
+    cout << "Enter today's temperature in Celsius: ";  // Ask the user to enter today's temperature in Celsius
     cin >> temperature;
 
-    if (temperature >= 30) {
+    if (temperature >= 30) { // Decide the price based on the weather
         cout << "It's hot! Lemonade costs $0.80 today." << endl;
     } else {
         cout << "Lemonade costs $1.00 today." << endl;
