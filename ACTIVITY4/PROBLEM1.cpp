ec17f95e5ced786c7fc239acb5320d8831c20f59
scyaena <smpurisima@gbox.adnu.edu.ph>
2025-10-05 22:13:40 +0800

    PROBLEM1.cpp

--- ACTIVITY4/PROBLEM1.cpp
+++ ACTIVITY4/PROBLEM1.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main() {
     int money;
 
-    cout << "Enter your money: ";
+    cout << "Enter your money: "; // Ask the user for the input
     cin >> money;
 
-    if (money >= 1.0) {
-        cout << "Enjoy your lemonade!" << endl;
+    if (money >= 1.0) {  // Decision-making using if statement
+
+        cout << "Enjoy your lemonade!" << endl; // If the user has 1 or more dollars
     } else {
-        cout << "Sorry, you need more money." << endl;
+        cout << "Sorry, you need more money." << endl; // If the user has less than 1 dollar
     }
 
     return 0;
